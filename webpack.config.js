60d3d1b4fce5f6c9956c889f7c49112a8bba2982
knowater521 <lxn740242108@outlook.com>
2020-04-11 15:30:58 +0800

    initial

---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+const path = require('path');
+const webpack =require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin =require('extract-text-webpack-plugin');
+module.exports = {
+    entry: './src/app.jsx',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/dist/',
+        filename: 'js/app.js'
+    },
+    module: {
+        rules: [
+            //react语法的处理
+            {
+                test: /\.m?jsx$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env', 'react']//@babel/preset-
+                    }
+                }
+            },
+            //css文件的处理
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+                //use: [
+                    //'style-loader',
+                    //'css-loader'
+                //]
+            },
+            //sass文件的处理
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
+            //images
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                            name: 'resource/[name].[ext]'
+                        }
+                    }
+                ]
+            },
+            //fonts
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|otf)$/i,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                            name: 'resource/[name].[ext]'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        //html文件的处理
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        //独立css文件
+        new ExtractTextPlugin("css/[name].css"),//styles
+        //提出公共模块
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'common',
+            filename: 'js/base.js'
+        })
+    ],
+    devServer: {
+        port: 8086
+        //contentBase: './dist'
+   }
+};
